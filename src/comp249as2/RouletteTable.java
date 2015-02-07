5b2f9ad4d272bc9d5ed094ab2eb93dbebbc06d8d
Kenny Jiang <sirkuanjiang@gmail.com>
2015-02-07 11:55:22 -0500

    update 11:55 am feb 07

--- src/comp249as2/RouletteTable.java
+++ src/comp249as2/RouletteTable.java
@@ -3,16 +3,25 @@ package comp249as2;
 import java.util.*;
 
 public class RouletteTable {
-	
+
 	Random Randomize = new Random();
+
+	int number;
 	
-	public double Spin(){
-		double number = Randomize.nextInt();
-		
-		while (number)
-		
+	int snumber = Randomize.nextInt(10);
+
+
+	public double SpinEuro(){
+
+
+			number = Randomize.nextInt();
+
+
+		System.out.println(number);
+
+
 		return number;
 	}
-	
+
 
 }
